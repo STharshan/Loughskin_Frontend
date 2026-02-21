dc362221db89c3081aa4cfb634c669f3facfed5c
STharshan <sivatharshan81@gmail.com>
2026-02-21 22:47:49 +0530

    service

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import Navigation from "./components/Navbar";
 import Footer from "./components/Footer";
+import Service from "./page/Service";
+import About from "./page/About";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/page/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AboutPage from '../components/About/Aboutsection'
+import OurValue from '../components/About/OurValue'
+import Banner from '../components/Banner'
+
+const About = () => {
+  return (
+    <div>
+      <AboutPage />
+      <OurValue />
+      <Banner />
+    </div>
+  )
+}
+
+export default About
